9af02d605bb3c420191cafbc34283d5fb3da6285
jimgo188 <50714117+jimgo188@users.noreply.github.com>
2026-09-03 14:23:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,3 +2,9 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+git clone -b files https://github.com/jimgo188/op-package files
+
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
