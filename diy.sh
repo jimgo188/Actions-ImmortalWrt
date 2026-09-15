47f10a046e1b08d026d99f36f72343d4448efafa
jimgo188 <50714117+jimgo188@users.noreply.github.com>
2026-09-15 16:12:22 +0800

    Modify diy.sh to update package sources
    Updated package sources and removed old golang package.

--- diy.sh
+++ diy.sh
@@ -5,6 +5,9 @@
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 git clone -b files https://github.com/jimgo188/op-package files
 
+# mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
